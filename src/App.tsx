c8b690c00a98ea66bc5fd320dae185db877ef4fc
Demmynile <balogun80@gmail.com>
2022-09-22 08:06:05 +0100

    adding more functionality to the advanced cart

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import {Home} from './pages/Home'
 import {Store} from './pages/Store'
 import {About} from './pages/About'
 import {Navbar} from './components/Navbar'
+import { ShoppingCartProvider } from './context/ShoppingCartContext'
 
 
 
@@ -12,7 +13,8 @@ function App() {
 
   return (
     <>
-<Navbar/>
+    <ShoppingCartProvider>
+   <Navbar/>
    <Container className = "mb-4">
      <Routes>
       <Route path = "/" element={<Home />} />
@@ -20,6 +22,7 @@ function App() {
        <Route path = "/about" element={<About />} />
     </Routes>
   </Container>
+  </ShoppingCartProvider>
   </>
 
    
